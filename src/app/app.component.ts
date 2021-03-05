234c8359e33f07b6a9972d8bc49029f2ef8ea016
kanika10xtech <kanika@10xtech.co.in>
2021-03-05 17:49:03 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,18 @@ export class AppComponent {
   name = "Angular " + VERSION.major;
   version = VERSION.major;
 
+  tasks: any[] = [];
+  hasTask = false;
+  constructor() {}
   getClass() {
     return this.version == "11" ? "bold" : "green";
+  }
+
+  generateTaskList() {
+    this.tasks = [];
+    for (var i = 1; i < 12; i++) {
+      this.tasks.push("Task" + i);
+    }
+    this.hasTask = this.tasks.length > 0;
   }
 }
